0169aba07bffa33c93b4e7bf1cc1b1acb3419182
manski117 <choromanski117@gmail.com>
2023-01-17 18:12:21 -0500

    Add conditional rendering to main content
    Main content now renders conditionally based on the boolean value
    of onFormPage stored in App() state. Next step is to link it to a
    button's behavior that lives inside the Header component.

--- cv-form-odin-project/src/App.js
+++ cv-form-odin-project/src/App.js
@@ -3,15 +3,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Form from './components/Form';
 import Resume from './components/Resume';
+import React from 'react';
 
 
 function App() {
+  //control which page is rendered to main
+  const [onFormPage, setOnFormPage] = React.useState(true);
+
+  const currentPage = onFormPage ? <Form /> : <Resume />
+
   return (
     <div className="app-container">
       <Header />
       <div className="main-content">
-        <Form />
-        <Resume />
+        {currentPage}
+        
       </div>
 
       <Footer />
--- cv-form-odin-project/src/components/Header.js
+++ cv-form-odin-project/src/components/Header.js
@@ -3,7 +3,13 @@ import React from "react"
 function Header() {
     return (
         <header className="header">
-            <h1>Header</h1>
+            <div className="button-wrapper">
+                <button>Generate CV</button>
+                <button>Edit CV</button>
+            </div>
+            <h1>CV Generator App</h1>
+            <h3>by Manski117</h3>
+
         </header>
     )
 }
