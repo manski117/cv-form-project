8b50b9cce8a008833c6cae8c3b3fa7a194c66402
manski117 <choromanski117@gmail.com>
2023-01-18 12:18:58 -0500

    Complete EduForm basic functionality
    Updated the CSS to make form sections a bit more readable and also
    finished building out the EduForm component. Basic functionality
    is working well with state from grandparent context and everything
    is running smoothly.

--- cv-form-odin-project/src/components/EduForm.js
+++ cv-form-odin-project/src/components/EduForm.js
@@ -5,14 +5,6 @@ import { FormContext } from "../App";
 function EduForm() {
     const {formData, setFormData} = React.useContext(FormContext);
     
-    // const [formData, setFormData] = React.useState(
-    //     {
-    //         school: '',
-    //         degree: '',
-    //         startDate: '',
-    //         gradDate: '',
-    //     }
-    // )
 
     function handleChange (event){
         //get the event object from the element and destructure it
@@ -54,6 +46,30 @@ function EduForm() {
                     placeholder="Degree Earned"
                     onChange={handleChange}
                     value={formData.degree}
+                     />
+            </label>
+            <br />
+            <label htmlFor="startDate">Date Started 
+                <input 
+                    id="startDate" 
+                    name="startDate" 
+                    type="text" 
+                    className="form-input" 
+                    placeholder="Year Started"
+                    onChange={handleChange}
+                    value={formData.startDate}
+                     />
+            </label>
+            <br />
+            <label htmlFor="gradDate">Date Started 
+                <input 
+                    id="gradDate" 
+                    name="gradDate" 
+                    type="text" 
+                    className="form-input" 
+                    placeholder="Year of Graduation"
+                    onChange={handleChange}
+                    value={formData.gradDate}
                      />
             </label>
         </div>
